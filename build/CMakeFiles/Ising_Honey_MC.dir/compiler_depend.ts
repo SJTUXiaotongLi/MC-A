888f0ba78ea83c7f407bfcf81181d33f60207e90
Joe <125941991+emmmmmmmnmm@users.noreply.github.com>
2023-05-25 17:17:39 +0800

    Delete build directory

--- build/CMakeFiles/Ising_Honey_MC.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for Ising_Honey_MC.
--- build/CMakeFiles/Ising_Square_exact.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Ising_Square_exact.
-# This may be replaced when dependencies are built.
