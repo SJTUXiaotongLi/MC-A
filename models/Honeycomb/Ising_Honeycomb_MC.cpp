55f1f7011747bcfd0d6ed4ddef6a0919c7f3c702
Joe <125941991+emmmmmmmnmm@users.noreply.github.com>
2023-05-25 17:16:01 +0800

    Add files via upload

---
+++ models/Honeycomb/Ising_Honeycomb_MC.cpp
@@ -0,0 +1,51 @@
+#include <random>
+#include <iostream>
+#include "Ising_Honeycomb.hpp"
+#include <fstream>
+
+int main(int argc, char **argv)
+{
+	std::cout << "# MC simulation (Honeycomb lattice)" << std::endl;
+	if (argc <= 1) {
+		std::cerr << "We need a parameter file (see sample_parameters/...)" << std::endl;
+		exit(1);
+	}
+	HoneycombLatticeParameterBundle_MC parameter(argv[1], true);
+	
+	/* Honeycomb lattice system */
+	const int dim = 2;
+	std::vector<int> L(dim);
+	L[0] = parameter._L0();
+	L[1] = parameter._L1();
+	const int n_sites = HoneycombLatticeIsingSystem::eval_n_spins(L);
+
+	/* list of temperatures */
+	const double Tmin = parameter._Tmin();
+	const double Tmax = parameter._Tmax();
+	const int nT = parameter._N_TemperaturePoints();
+	const double Tdlt = (Tmax - Tmin) / (nT - 1);
+	std::vector<double> beta(nT);
+	for (int i = 0; i < nT; i++) beta[i] = 1.0 / (Tmin + i * Tdlt);
+	
+	/* MC parameters */
+	const int n_bins = parameter._n_bins();
+	const int mcs_thermalization = parameter._mcs_thermalization();
+	const int n_samples_per_bin = parameter._n_samples_per_bin();
+	const int mcs_interval_btwn_bins = parameter._mcs_interval_btwn_bins();
+	
+	/* simulation */
+	const int seed = 745099137;
+	RandomNumberGenerator mtwist(seed, n_sites);
+	IsingSystem::setup_RNGen(mtwist);
+	HoneycombLatticeDataBundle data_bundle(n_sites, beta, n_bins, n_samples_per_bin);
+	HoneycombLatticeIsingSystem system(L, data_bundle, mcs_thermalization, mcs_interval_btwn_bins);
+	
+	std::ofstream file("q.txt");
+	std::streambuf* backup = std::cout.rdbuf();
+	std::cout.rdbuf(file.rdbuf());  
+
+	data_bundle.output_legends_MC(system._system_size());
+	system.run_MC();
+	
+	return 0;
+}
